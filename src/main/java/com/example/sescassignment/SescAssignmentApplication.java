f7395ce0ab9a9b2373e4ba6c0c0d2c61ac452c3a
sw1ssyy <johnanderson8484@gmail.com>
2023-03-21 10:49:22 +0000

    First Commit

---
+++ src/main/java/com/example/sescassignment/SescAssignmentApplication.java
@@ -0,0 +1,13 @@
+package com.example.sescassignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SescAssignmentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SescAssignmentApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/sescassignment/SescAssignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sescassignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SescAssignmentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
